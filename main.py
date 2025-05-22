23ad5d2305c4885e93b80a9fc802c806ac618ade
YelloooBlue <YelloooBlue@outlook.com>
2025-05-22 13:11:28 +0800

    input class ipynb

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 def main():
     print("Hello from pytorchstudy!")
 
+    import torch
+    if torch.backends.mps.is_available():
+        mps_device = torch.device("mps")
+        x = torch.ones(1, device=mps_device)
+        print (x)
+    else:
+        print ("MPS device not found.")
+
 
 if __name__ == "__main__":
     main()
