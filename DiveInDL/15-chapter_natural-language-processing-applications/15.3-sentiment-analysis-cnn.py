bcfc29f6907d4a32ba7b0646992fb0cecb94945d
YelloooBlue <YelloooBlue@outlook.com>
2025-07-12 13:01:34 +0800

    15.3加入一些注释

--- DiveInDL/15-chapter_natural-language-processing-applications/15.3-sentiment-analysis-cnn.py
+++ DiveInDL/15-chapter_natural-language-processing-applications/15.3-sentiment-analysis-cnn.py
@@ -161,34 +161,50 @@ class TokenEmbedding:
 # =========================== 网络定义 ===========================
 
 class TextCNN(nn.Module):
-    def __init__(self, vocab_size, embed_size, kernel_sizes, num_channels,
-                 **kwargs):
+    def __init__(self, vocab_size, embed_size, kernel_sizes, num_channels, **kwargs):
         super(TextCNN, self).__init__(**kwargs)
+        
         self.embedding = nn.Embedding(vocab_size, embed_size)
-        # 这个嵌入层不需要训练
-        self.constant_embedding = nn.Embedding(vocab_size, embed_size)
+        self.constant_embedding = nn.Embedding(vocab_size, embed_size) # 这个嵌入层不需要训练
+       
         self.dropout = nn.Dropout(0.5)
         self.decoder = nn.Linear(sum(num_channels), 2)
         # 最大时间汇聚层没有参数，因此可以共享此实例
         self.pool = nn.AdaptiveAvgPool1d(1)
         self.relu = nn.ReLU()
+        
         # 创建多个一维卷积层
         self.convs = nn.ModuleList()
         for c, k in zip(num_channels, kernel_sizes):
-            self.convs.append(nn.Conv1d(2 * embed_size, c, k))
+            self.convs.append(nn.Conv1d(2 * embed_size, c, k)) # 这个卷积层的输入通道数是2 * embed_size，输出通道数是c，卷积核的大小是k
+
+        """
+            不同kernel_sizes宽度的卷积核可以捕获不同数目的相邻词元之间的局部特征
+        """
 
     def forward(self, inputs):
         # 沿着向量维度将两个嵌入层连结起来，
         # 每个嵌入层的输出形状都是（批量大小，词元数量，词元向量维度）连结起来
-        embeddings = torch.cat((
-            self.embedding(inputs), self.constant_embedding(inputs)), dim=2)
+        embeddings = torch.cat((self.embedding(inputs), self.constant_embedding(inputs)), dim=2)
+        
         # 根据一维卷积层的输入格式，重新排列张量，以便通道作为第2维
         embeddings = embeddings.permute(0, 2, 1)
+
         # 每个一维卷积层在最大时间汇聚层合并后，获得的张量形状是（批量大小，通道数，1）
         # 删除最后一个维度并沿通道维度连结
         encoding = torch.cat([
             torch.squeeze(self.relu(self.pool(conv(embeddings))), dim=-1)
             for conv in self.convs], dim=1)
+        
+        """
+            embeddings的形状是（批量大小，词元向量维度，时间步数=词元数量），
+            conv(embeddings)的输出形状是（批量大小，通道数，时间步数），「从嵌入空间到多通道空间」
+            self.pool(conv(embeddings))的输出形状是（批量大小，通道数，1），
+            torch.squeeze(self.pool(conv(embeddings)), dim=-1)的输出形状是（批量大小，通道数），
+            torch.cat([...], dim=1)的输出形状是（批量大小，通道数总和），
+        """
+
+        # 通过全连接层进行分类
         outputs = self.decoder(self.dropout(encoding))
         return outputs
     
